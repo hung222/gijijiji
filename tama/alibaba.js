fb1478c9b0720785fa042fa99de0ba412b535fa9
jomokio <khuykuy@gmail.com>
2022-10-29 21:32:00 +0700

    upload thu muc tama vo github

--- tama/alibaba.js
+++ tama/alibaba.js
@@ -1,1 +1,57 @@
-CONSOLE.LOG("alibababababab cái quần mada rách ra làm 3")
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+app.listen(process.env.PORT || 3000);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'))
+app.use(express.static('files'))
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+app.get('/', function(req, res){
+    res.render('index', { title: 'Hey', message: 'dragonbound dragonbound!' })
+ });
+const mongoAtlasUri = "mongodb+srv://gh:<mnbvcxzasd>@cluster0.szoaq.mongodb.net/?retryWrites=true&w=majority";
+
+try {
+    // Connect to the MongoDB cluster
+    mongoose.connect(
+        mongoAtlasUri,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        () => console.log(" Mongoose blog mindx is connected")
+    );
+} catch (e) {
+    console.log("could not connect");
+}
+module.exports = app;
